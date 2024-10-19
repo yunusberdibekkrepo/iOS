fa19de03f4329e0a987a81b31676c6bd0adeba8c
Yunus Emre Berdibek <com.yunusberdibekk@gmail.com>
2024-10-20 01:03:01 +0300

    feat: Shake sign in button if login fails

--- Bankey/Bankey/Scenes/Login/LoginViewController.swift
+++ Bankey/Bankey/Scenes/Login/LoginViewController.swift
@@ -185,6 +185,19 @@ private extension LoginViewController {
     func configureErrorLabel(withMessage message: String) {
         errorMessageLabel.isHidden = false
         errorMessageLabel.text = message
+
+        shakeButton()
+    }
+
+    func shakeButton() {
+        let animation = CAKeyframeAnimation()
+        animation.keyPath = "position.x"
+        animation.values = [0, 10, -10, 10, 0]
+        animation.keyTimes = [0, 0.16, 0.5, 0.83, 1]
+        animation.duration = 0.4
+
+        animation.isAdditive = true
+        signInButton.layer.add(animation, forKey: "shake")
     }
 }
 
